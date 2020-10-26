d75bf442b58ad836ad17ad15d85661fa9f62025a
Domovenokkuzya <72493862+Domovenokkuzya@users.noreply.github.com>
2020-10-26 13:08:01 +0300

    Add files via upload

---
+++ Laboratornaya_2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Net.NetworkInformation;
+namespace Laboratornaya_2
+{
+    interface IPrint
+    {
+        void Print();
+    }
+    public abstract class Geometricalfigure
+    {
+        public abstract string Square();
+        public string Type
+        {
+            get
+            {
+                return this._Type;
+            }
+            protected set
+            {
+                this._Type = value;
+            }
+        }
+        string _Type;
+        public override string ToString()
+        {
+            return this.Type + " площадью " + this.Square().ToString();
+        }
+
+    }
+
+    public class Rectangle : Geometricalfigure, IPrint
+    {
+        private double Width;
+        private double Height;
+        public Rectangle(double W, double H)
+        {
+            this.Width = W;
+            this.Height = H;
+            this.Type = "Прямоугольник";
+        }
+        public override string Square()
+        {
+            return (Width * Height).ToString();
+        }
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+
+    }
+
+    public class Box : Rectangle, IPrint
+    {
+        private double Width;
+        public Box(double size) : base(size, size)
+        {
+            this.Width = size;
+            this.Type = "Квадрат";
+        }
+        public override string Square()
+        {
+            return (Width * Width).ToString(); ;
+        }
+        
+
+    }
+    public class Circle : Geometricalfigure, IPrint
+    {
+        public double Radius;
+        double pi = 3.14;
+        public Circle(double r)
+        {
+            this.Radius = r;
+            this.Type = "Круг";
+        }
+        public override string Square()
+        {
+            return (pi * Radius * Radius).ToString();
+        }
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(5, 4);
+            Box b = new Box(5);
+            Circle c = new Circle(5);
+            r.Print();
+            b.Print();
+            c.Print();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Laborotornaya_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using static System.Math;
+
+namespace Laborotornaya_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ФИО:Плотников Федор Сергеевич, Группа:ИУ5-31Б");
+            Console.WriteLine("Введите коэффициенты биквадратного уравнения");
+            int A, B, C;
+            double D;
+            string s;
+            s = Console.ReadLine();
+            A = int.Parse(s);
+            if (A is 0)
+            {
+                Console.WriteLine("Введите число A, не равное 0");
+                s = Console.ReadLine();
+                A = int.Parse(s);
+            }
+            s = Console.ReadLine();
+            B = int.Parse(s);
+            s = Console.ReadLine();
+            C = int.Parse(s);
+            D = B * B - 4 * A * C;
+            if(D is 0)
+            {
+                double x1;
+                x1 = -B / (2 * A);
+                if (x1 < 0)
+                    D = -1;
+                if (x1 > 0)
+                {
+                    x1 = Math.Sqrt(x1);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("x1={0},x2={1}", x1, -x1);
+                    Console.ResetColor();
+                }
+            }
+            if (D > 0)
+            {
+                double x1, x2;
+                x1 = (-B + Math.Sqrt(D)) / (2 * A);
+                if (x1 > 0)
+                {
+                    x1 = Math.Sqrt(x1);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("x1={0:F1},x2={1:F1}", x1, -x1);
+                    Console.ResetColor();
+                }
+                x2 = (-B - Math.Sqrt(D)) / (2 * A);
+                if (x2 > 0)
+                {
+                    x2 = Math.Sqrt(x2);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("x3={0:F1},x4={1:F1}", x2, -x2);
+                    Console.ResetColor();
+                }
+                if (x1 < 0 && x2 < 0)
+                    D = -1;
+            }
+            if (D < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine("КОРНЕЙ НЕТ");
+                Console.ResetColor();
+            }
+        }
+    }
+}
